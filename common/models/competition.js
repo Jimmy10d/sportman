85279cf82994c22282d0e9f30e3a39f219e0b1a8
Jimmy Chen <jimmy600d@gmail.com>
2017-03-27 23:36:56 +0700

    addRegister to competition

--- common/models/competition.js
+++ common/models/competition.js
@@ -1,5 +1,47 @@
-'use strict';
-
 module.exports = function(Competition) {
-
+	Competition.addRegister = function(UserId, CompetitionId, cb){
+		Competition.findOne({where:{id: CompetitionId}},
+			function(err,instance){
+				if(instance===null){
+					cb(null,null);
+				}else{
+					data = instance['register']; //get everyone who has like this Competition
+					theRegistersNow = data.toString();
+					//if UserId has like this Competition
+					if(theRegistersNow.includes(UserId)){
+						cb(null,instance);
+					}
+					//if this is the first Competition he see
+					else if(theRegistersNow === ''){
+						theRegistersNow = UserId;
+						Competition.updateAll({id: CompetitionId}, {register: theRegistersNow}, //update
+						function(err,info){
+							Competition.findOne({where:{id: CompetitionId}},
+								function(err,instance){
+									if(instance===null){
+										cb(null,null);
+									}else{
+										cb(null,instance);
+									}
+								})
+						});
+					}
+					//it's only the last Competition he's seen
+					else{
+						theRegistersNow = theRegistersNow + ',' + UserId;
+						Competition.updateAll({id: CompetitionId}, {register: theRegistersNow}, //update
+						function(err,info){
+							Competition.findOne({where:{id: CompetitionId}},
+								function(err,instance){
+									if(instance===null){
+										cb(null,null);
+									}else{
+										cb(null,instance);
+									}
+								})
+						});
+					}
+				}				
+			});
+	};
 };
