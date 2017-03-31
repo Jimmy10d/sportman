59e6f44410ee5fb502357d2ec4a8fe2077f5e8a6
Jimmy Chen <jimmy600d@gmail.com>
2017-03-31 10:35:06 +0700

    add method to fixture

--- common/models/fixture.js
+++ common/models/fixture.js
@@ -1,5 +1,26 @@
 'use strict';
 
 module.exports = function(Fixture) {
+	Fixture.getFixture = function(fixtureNumber, cb){
+		Fixture.findOne({fields: {id: false}, where:{fixture_number:fixtureNumber}},
+			function(err,instance){
+				if(instance===null){
+					cb(null,null);
+				}else{
+					cb(null,instance);
+				}
+			});
+	};
 
+	Fixture.remoteMethod(
+		'getFixture',
+		{
+			accepts: {arg: 'fixtureNumber', type: 'string'},
+			returns: {arg: 'id', type: 'string', root: true},
+			http: {path: '/getFixture', verb: 'get', source: 'query'},
+			description: "Get fixture instance by fixture number"
+		}
+	);
 };
+
+
